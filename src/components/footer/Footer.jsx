edae52d81dd4147acb22fbc0d3ec8297abf3e7c9
sohailahmad-dev <sohailahmaddev@gmail.com>
2024-05-14 05:01:03 +0500

    changes in footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -25,50 +25,58 @@ export default function Footer() {
                     <Grid item sm={2.5} xs={12} >
                         <img src={logo} alt="logo" className='f-logo' />
                     </Grid>
-                    <Grid item sm={2.5} xs={6} >
-                        <div className="f-linkBold">Applications</div>
-                        <div className="f-link" onClick={() => handleNavigate('/')}>Partners</div>
-                        <div className="f-link" onClick={() => handleNavigate('/GuestRecognition')}>Guest Recognition</div>
-                        <div className="f-link" onClick={() => handleNavigate('/MobileMarketStrategies')}>Mobile Strategies</div>
-                        <div className="f-link" onClick={() => handleNavigate('/Personalization')}>Personalizaiton</div>
-                    </Grid>
-                    <Grid item sm={2.75} xs={6} >
-                        <div className="f-linkBold" style={{ visibility: 'hidden' }} >Application</div>
-                        <div className="f-link" onClick={() => handleNavigate('/MessageMarketSegmentation')}>Market Segmentation</div>
-                        <div className="f-link" onClick={() => handleNavigate('/GeoFencing')}>Geo Fencing</div>
-                        <div className="f-link" onClick={() => handleNavigate('/SpecialityMessaging')}>Messaging</div>
-                        <div className="f-link" onClick={() => handleNavigate('/TickeitngTours')}>Ticket Tours Activities</div>
+
+                    <Grid item sm={3.5} xs={6} >
+                        <div className="f-linkBold">Resources</div>
+                        <div className="f-link" onClick={() => handleNavigate('/')} >Help center</div>
+                        <div className="f-link-description">
+                            Ask our Help Desk Bot, if you need any information regarding your account.
+
+                            Click here to connect.
+                        </div>
+                        <div className="f-link" onClick={() => handleNavigate('/')} >Customer Hub</div>
+                        <div className="f-link-description">
+                            Sorry, it’s for our customers!
+
+                            Fill out this form and we will set up an appointment.
+
+                        </div>
+                        <div className="f-link" onClick={() => handleNavigate('/')} >Status</div>
+                        <div className="f-link-description">
+                            We are always up and we our connections are too.  If you are having problems please email us at
+                            <a href='mailto:service@iveew.co'> service@iveew.co</a>
+                        </div>
+
 
                     </Grid>
-                    <Grid item sm={2.25} xs={6} >
-                        <div className="f-linkBold" onClick={() => handleNavigate('/Pricing')} style={{visibility:'hidden'}}>P</div>
-                        <div className="f-link" onClick={() => handleNavigate('/About')}>About iVeew</div>
-                        <div className="f-link"
-                            onClick={() => setActive('Careers')}
-                            onMouseEnter={() => setActive('Careers')}
-                        >Careers</div>
-                        {active === 'Careers' && <div>
+                    <Grid item sm={3.5} xs={6} >
+                        <div className="f-linkBold">Useful Links</div>
+                        <div className="f-link">Careers</div>
+                        <div className='f-link-description'>
                             Check us out and if you think there is a match, email us at <a href='mailto:info@iveew.co'>info@iveew.co</a>
-                        </div>}
-                        <div className="f-link"
-                            onClick={() => setActive('Contact')}
-                            onMouseEnter={() => setActive('Contact')}
-                        >
+                        </div>
+                        <div className="f-link" onClick={() => handleNavigate('/Privacy')} >Privacy</div>
+                        <div className="f-link-description">
+                            California Consumer Privacy Act (CCPA
+
+                            EU's Digital Markets Act (DMA) March 2024
+
+                            GDPR (General Data Protection Regulation)
+
+                        </div>
+                        <div className="f-link">Partner portal</div>
+                        <div className='f-link-description'>
+                            Our partner portal connects our platforms and applications .  Fill out the form to  talk with us about our APIs or yours.  </div>
+
+                    </Grid>
+                    <Grid item sm={2.5} xs={6} >
+                        <div className="f-linkBold">Useful Links</div>
+
+                        <div className="f-link" onClick={() => handleNavigate('/About')}>About iVeew</div>
+                        <div className="f-link">
                             <a href='https://passtrek.com/#contact' target='_blank' className='f-link' style={{ textDecoration: 'none' }} >Contact</a>
                         </div>
-                        <div className="f-link"
-                            onClick={() => setActive('Partner portal')}
-                            onMouseEnter={() => setActive('Partner portal')}
-                        >Partner portal</div>
-                        {active === 'Partner portal' && <div>
-                            Our partner portal connects our platforms and applications .  Fill out the form to  talk with us about our APIs or yours.  </div>}
-                    </Grid>
-                    <Grid item sm={2} xs={6} >
-                        <div className="f-linkBold">Resources</div>
-                        <div className="f-link" onClick={() => handleNavigate('/')} >Customer Hub</div>
-                        <div className="f-link" onClick={() => handleNavigate('/')} >Help center</div>
-                        <div className="f-link" onClick={() => handleNavigate('/')} >Status</div>
-                        <div className="f-link" onClick={() => handleNavigate('/Privacy')} >Privacy</div>
+
                     </Grid>
                 </Grid>
                 <div className='f-bottom'>
